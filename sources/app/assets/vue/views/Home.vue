98a72aeffc10a2e6e5880f3b917ec100e1a69501
xcitic <xcitic@gmail.com>
2019-09-28 20:49:39 +0200

    VueRouter setup

---
+++ sources/app/assets/vue/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <div class="row col">
+    <h1>Home</h1>
+  </div>
+
+  <div class="row col">
+    <p>Landing page is here</p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+</style>
